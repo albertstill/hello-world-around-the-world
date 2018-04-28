322c848cfed1cb43cf42bc5d9b7abc609b94d613
Albert Still <albertjamesstill@gmail.com>
2018-04-28 15:30:24 +1000

    use docker

--- index.js
+++
@@ -1,9 +0,0 @@
-// const ping = require('node-icmp-traceroute');
-
-const express = require('express');
-
-const app = express();
-
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
