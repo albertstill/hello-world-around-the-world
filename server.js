f701d6368490197039ffdf1c07a7876191d110f7
Albert Still <albertjamesstill@gmail.com>
2018-08-22 18:02:53 +1000

    .

--- server.js
+++ server.js
@@ -1,33 +1,37 @@
 const express = require('express');
 const ping = require('node-icmp-traceroute');
 
-// Constants
 const PORT = 8080;
-const HOST = 'localhost';
+const HOST = '0.0.0.0';
 
-// App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello world\n');
+// https://stackoverflow.com/questions/22381783/traceroute-why-my-packets-travel-around-the-world
 
-//   ping.createSession().traceRoute('google.com', (err, data) => {
-//     if (err) {
-//       if (err.name === 'DNSError') console.log('err = ', err.message);
-//     } else {
-//       console.log(
-//         '[Inside app] data.latitude = ',
-//         data.latitude,
-//         ' data.longitude = ',
-//         data.longitude,
-//         ' data.source = ',
-//         data.source,
-//         ' data.target = ',
-//         data.target,
-//         ' status = ',
-//         data.status,
-//       );
-//     }
-//   });
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+
+app.get('/traceroute', (req, res) => {
+  const { ipToTraceroute } = req.query;
+
+  ping.createSession().traceRoute('google.com', (err, data) => {
+    if (err) {
+      if (err.name === 'DNSError') throw err;
+    } else {
+      console.log(
+        '[Inside app] data.latitude = ',
+        data.latitude,
+        ' data.longitude = ',
+        data.longitude,
+        ' data.source = ',
+        data.source,
+        ' data.target = ',
+        data.target,
+        ' status = ',
+        data.status,
+      );
+    }
+  });
 });
 
 app.listen(PORT, HOST);
