26e333e86dbe47e3b89522d78cf4b216a57b4e00
Albert Still <albertjamesstill@gmail.com>
2018-08-22 17:46:54 +1000

    Update server.js

--- server.js
+++ server.js
@@ -3,31 +3,31 @@ const ping = require('node-icmp-traceroute');
 
 // Constants
 const PORT = 8080;
-const HOST = '0.0.0.0';
+const HOST = 'localhost';
 
 // App
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello world\n');
 
-  ping.createSession().traceRoute('google.com', (err, data) => {
-    if (err) {
-      if (err.name === 'DNSError') console.log('err = ', err.message);
-    } else {
-      console.log(
-        '[Inside app] data.latitude = ',
-        data.latitude,
-        ' data.longitude = ',
-        data.longitude,
-        ' data.source = ',
-        data.source,
-        ' data.target = ',
-        data.target,
-        ' status = ',
-        data.status,
-      );
-    }
-  });
+//   ping.createSession().traceRoute('google.com', (err, data) => {
+//     if (err) {
+//       if (err.name === 'DNSError') console.log('err = ', err.message);
+//     } else {
+//       console.log(
+//         '[Inside app] data.latitude = ',
+//         data.latitude,
+//         ' data.longitude = ',
+//         data.longitude,
+//         ' data.source = ',
+//         data.source,
+//         ' data.target = ',
+//         data.target,
+//         ' status = ',
+//         data.status,
+//       );
+//     }
+//   });
 });
 
 app.listen(PORT, HOST);
